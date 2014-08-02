079676baf4f2c3251d55cd6bf3672e9c19f6a9ce
Widyanto Bagus Priambodo <bobby.priambodo@gmail.com>
2014-08-02 22:22:30 +0700

    added the Fragments to TabsPagerAdapter

--- src/com/bobbypriambodo/anisuke/adapter/TabsPagerAdapter.java
+++ src/com/bobbypriambodo/anisuke/adapter/TabsPagerAdapter.java
@@ -3,6 +3,8 @@ package com.bobbypriambodo.anisuke.adapter;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import com.bobbypriambodo.anisuke.BucketFragment;
+import com.bobbypriambodo.anisuke.FollowingFragment;
 
 /**
  * Created by Bobby Priambodo on 8/2/2014.
@@ -16,8 +18,8 @@ public class TabsPagerAdapter extends FragmentPagerAdapter {
 	@Override
 	public Fragment getItem(int i) {
 		switch (i) {
-			case 0: return null; // return FollowingFragment();
-			case 1: return null; // return BucketFragment();
+			case 0: return new FollowingFragment();
+			case 1: return new BucketFragment();
 			default: return null;
 		}
 	}
