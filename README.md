c62f281135ab1b046e22e059e7a38d4e2d3da920
Widyanto Bagus Priambodo <bobby.priambodo@gmail.com>
2014-08-02 21:19:32 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+anisuke
+=======
+
+Anisuke (Anime Scheduler) is an Android app for managing your anime download list.
---
+++ src/com/bobbypriambodo/anisuke/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bobbypriambodo.anisuke;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Bobby Priambodo on 8/2/2014.
+ */
+public class MainActivity extends FragmentActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
